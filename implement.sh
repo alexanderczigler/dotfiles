1c72f43a123ef871ad84d31acff8e06fe5f10bc7
ilix <src@ilix.se>
2016-10-31 06:39:30 +0100

    Created a rudimentary update-from-git procedure

--- implement.sh
+++ implement.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-mkdir ~/.ssh
+# Settings
+SOURCE="/home/ilix/Source/home"
+ORIGIN=`pwd`
+
+# Go to source location
+cd $SOURCE
+
+# Update
+git pull origin master
+
+# Setup ssh stuff
+mkdir -p ~/.ssh
 cp .ssh/config ~/.ssh/config
 
+# Setup bashrc
 cp .bashrc ~/.bashrc
+
+# Go back to where you were
+cd $ORIGIN
