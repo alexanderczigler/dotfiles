e34f7df0c3d84cc6ffe63e77546fbc6a408b68fe
Alexander Czigler <alexanderczigler@ysera.internetbolaget.nu>
2021-06-15 12:17:47 +0200

    fix(install): fix warning when bashrc file is missing

--- install.bash
+++ install.bash
@@ -4,4 +4,9 @@ BASHRC="$HOME/.bashrc"
 PWD=`pwd`
 SOURCE_LINE="source $PWD/bash/_include.bash"
 
+if [ ! -z $BASHRC ]; then
+    echo " - [x] No bashrc found, creating empty file at $BASHRC"
+    touch $BASHRC
+fi;
+
 grep -q -F "$SOURCE_LINE" $BASHRC || echo "$SOURCE_LINE" >> $BASHRC
