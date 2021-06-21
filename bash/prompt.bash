de0df84164c6fc279e1d101b5cdf75d32d7ca012
Alexander Czigler <alexander@iteam.se>
2021-06-21 10:37:17 +0200

    fix(env): direnv

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -11,7 +11,7 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 # direnv
 _direnv_hook() {
   local previous_exit_status=$?;
-  eval "$("/usr/bin/direnv" export bash)";
+  eval "$("direnv" export bash)";
   return $previous_exit_status;
 };
 if ! [[ "$PROMPT_COMMAND" =~ _direnv_hook ]]; then
